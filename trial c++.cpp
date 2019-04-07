76ad87ec18089f0477094e98e3b6415b8066737e
tahaabid <47201449+tahaabid@users.noreply.github.com>
2019-04-07 23:34:03 +0800

    Update trial c++.cpp
    Display function added

--- trial c++.cpp
+++ trial c++.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
 using namespace std;
 
+
+int menu() {
+  int choice;
+  cout << "*************************************************" << endl;
+  cout << "***Leo & Taha's 82 Inventory Management System***" << endl;
+  cout << "******--What would you like to do today?--******" << endl;
+  cout << "******    Choose an option from below    ******" << endl;
+  cout << "1. Add a new product" << endl;
+  cout << "2. Update Existing Product" << endl;
+  cout << "2. Update Existing Product" << endl;
+  cout << "3. Check Inventory" << endl;
+
+  cin >> choice;
+  return choice;
+}
+
 int main()
 {
   cout << "Hello World" << endl;
   return 0;
  }
+
 
 // updated message 23321321321
 // new shittttttt
